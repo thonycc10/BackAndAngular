7786273a1b8dc45c0dcc95ed0da9245781540f24
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-02-21 17:43:20 -0500

    AC: configuracion CORS

--- src/main/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/auth/ResourceServerConfig.java
+++ src/main/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.thony.spring.boot.backend.apirest.springbootbackendapirest.auth;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -13,11 +22,33 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     // se comenta para que se añada las validaciones desde el controlador anotaciones.
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll()
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/imgen/**").permitAll()
                 /* .antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN") // asignamos roles
                 .antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN") // asignamos roles
                 .antMatchers("/api/clientes/**").hasRole("ADMIN") */ // asignamos de frente para los demas metodosHTTP
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
