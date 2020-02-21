a52a92fbf6cf471f0a32dfc75bd2a909ba3fee99
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-02-21 16:42:47 -0500

    AC: agregando seguridad a nuestra rutas ResourceServerConfig

--- src/main/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/auth/ResourceServerConfig.java
+++ src/main/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/auth/ResourceServerConfig.java
@@ -10,9 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // se comenta para que se añada las validaciones desde el controlador anotaciones.
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes").permitAll()
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll()
+                /* .antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN") // asignamos roles
+                .antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN") // asignamos roles
+                .antMatchers("/api/clientes/**").hasRole("ADMIN") */ // asignamos de frente para los demas metodosHTTP
                 .anyRequest().authenticated();
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -30,11 +30,10 @@ INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'tho
 INSERT INTO clientes (region_id, name, apellido, email, create_at) VALUES(1,'gre', 'carrascp', 'thony@gmail.com', '2018-01-01');
 
 
-INSERT INTO roles (nombre) VALUES('ROLE_USER');
 INSERT INTO roles (nombre) VALUES('ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES('ROLE_USER');
 INSERT INTO usuarios (username, password, enable, nombre, apellido, email) VALUES('thony', '$2a$10$gfNdof46y.mfBpScJhIaMeL.sy3EO/.APsoqnFl7UReEwl9.4bi5e', true, 'test', 'test', 'test@gmail.com');
 INSERT INTO usuarios (username, password, enable, nombre, apellido, email) VALUES('lau', '$2a$10$zSQ7OtAnEKQoBAxOCc4DtuoBLpMChOE7ghB9zH8aJjlHiqY9wc4Su', true, 'test1', 'test1', 'test1@gmail.com');
 INSERT INTO user_athority (user_id, rol_id) VALUES(1,1);
 INSERT INTO user_athority (user_id, rol_id) VALUES(2,2);
-INSERT INTO user_athority (user_id, rol_id) VALUES(2,1);
 
