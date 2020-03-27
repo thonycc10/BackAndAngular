e94360b55c61c1e96d8bdcb66367a0b4ac4f565c
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-03-27 15:00:41 -0500

    crear metodo status ok GET

---
+++ src/main/java/com/thony/spring/boot/backend/apirest/springbootbackendapirest/controller/FacturaRestController.java
@@ -0,0 +1,22 @@
+package com.thony.spring.boot.backend.apirest.springbootbackendapirest.controller;
+
+import com.thony.spring.boot.backend.apirest.springbootbackendapirest.entity.Factura;
+import com.thony.spring.boot.backend.apirest.springbootbackendapirest.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class FacturaRestController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/facturas/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Factura get(@PathVariable Long id) {
+        return clienteService.findFacturadById(id);
+    }
+}
